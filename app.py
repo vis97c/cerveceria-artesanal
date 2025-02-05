2fd1401cc3a430b68e63e894902f1bc9e826d8b2
Victor Saa <vis97c@hotmail.com>
2025-02-05 17:25:51 -0500

    configurar menus

--- app.py
+++ app.py
@@ -9,3 +9,19 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/productos')
+def productos():
+    return render_template('productos.html')
+
+@app.route('/clientes')
+def clientes():
+    return render_template('clientes.html')
+
+@app.route('/ventas')
+def ventas():
+    return render_template('ventas.html')
+
+@app.route('/facturacion')
+def facturacion():
+    return render_template('facturacion.html')
