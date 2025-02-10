c638c8c80589b9d0edb655f180cd50e63246cb80
aangiehurtado <112356162+aangiehurtado@users.noreply.github.com>
2025-02-10 10:03:40 -0500

    Update clientes.py
    Documentación Técnica de CLIENTES

--- modules/clientes.py
+++ modules/clientes.py
@@ -16,18 +16,18 @@ def clientes(correr):
    """
     )
 
-    # Crear nuevo cliente.
+    # Función que crea un nuevo cliente. Solicitando los 6 argumentos, (número de identificación (ID), nombre, apellido, dirección, teléfono, correo electrónico)
     def crear(valores):
         correr("INSERT INTO clientes VALUES (?, ?, ?, ?, ?, ?)", valores)
 
-    # Actualizar direccion de un cliente existente.
+    # Función que actualiza dirección de un cliente existente. Solicitando 2 argumentos (ID cliente y nueva dirección) 
     def actualizarDireccion(identificador, nuevoDireccion):
         correr(
             "UPDATE clientes SET direccion = ? WHERE id = ?",
             (nuevoDireccion, identificador),
         )
 
-    # Consultar un cliente por su ID.
+    # Función que consulta la información de un cliente. A partir del ID del cliente, muestra la información vigente. Para esto se requiere un objeto cursor 
     def consultarUno(identificador):
         cursorObj = correr(
             "SELECT * FROM clientes WHERE id = ?", (identificador,), False
